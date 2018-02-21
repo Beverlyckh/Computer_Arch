ec58e1a8595b122ac2635a34986c6d98ecac09b3
Beverly_ckh <beverly99@hotmail.fr>
2018-02-20 22:51:13 -0800

    update

--- CArchitecture_Assignment.c
+++ CArchitecture_Assignment.c
@@ -1,103 +1,107 @@
 /*
  ============================================================================
  Name        : CArchitecture_Assignment.c
- Author      : 
+ Author      : Beverly ACKAH
  Version     :
  Copyright   : Your copyright notice
  Description : Hello World in C, Ansi-style
  ============================================================================
  */
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 struct date{
-	int day;
-	int month;
+	char *day[2];
+	char *month[2];
 	int year;
 
 };
-
 struct contact{
 	char name[15];
 	char lastname[15];
-	char bdi[10]; //bdi = birthday information
+	int bdi[10]; //bdi = birthday information
 };
+//global variable that can be accessed by main
+struct contact c[6];
 
+//load contact function to read all records of contacts.txt
 void loadContacts(){
-	//char** tokens;
 	char filename[100];
-
-	struct contact c;
+	struct contact *array;
+	struct date *date1;
 
 	FILE *file = fopen("/Users/beverlyackah/Desktop/Bev's_Workspace_DataStructure2017/CArchitecture_Assignment/contacts.txt", "r");
 
 	char line[256];
+	int count = 0;
 
 	while(fgets(line, sizeof(line), file)){
 
-		printf("%s", line);
-
 		char *token = strtok(line, " ");
 		int tokenCount = 0;
-		char *fName[15];
-		char *lName[15];
-		char *dob[15];
+		char *fName[16];
+		char *lName[16];
+		int *dob[10];
+		struct contact myContact;
 		while (token != NULL)
 		{
-			//printf("The token is %s\n", token);
-
-
-
 			tokenCount ++;
-
-
-			//printf("The number of count is %d"
-			//		"\n", tokenCount);
-
 			if(tokenCount == 1){
-				strcpy(fName,token);
 
+				strcpy(myContact.name, token);
 			} else if(tokenCount == 2){
-				strcpy(lName,token);
+
+				strcpy(myContact.lastname, token);
 
 			} else if (tokenCount == 3){
-				strcpy(dob,token);
+
+				strcpy(myContact.bdi,token);
+				break;
 			}
 			else{
-
 			}
-
 			token = strtok(NULL, " ");
+
 		}
 
-		printf("The token is first : %s  last : %s bDay: %s\n", fName,lName,dob);
+		c[count]=myContact;
 
-		//struct at the end of my for loop
-
-		struct contact contactObject(fName, lName, dob);
-
-
+		count++;
 	}
-
-
-
 	fclose(file);
-
-
-
 }
 
-
 int main(void) {
-	//puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
-
-	struct date date_info[100];
-	struct date *dates= date_info;
-
-	struct contact contact_info[100];
-	struct contact *contacts=contact_info;
-
+	struct date myDate;
 	loadContacts();
+	int i;
+	for(i=0; i<6; i++){
+		//to split the birthday information we got in token
+		char *token1 = strtok(c[i].bdi, "-"); //returns first token (token = birtday info we get from for loop)
+		int tokenCount = 0;
+		int *bDay;
+		int *bMonth;
+		int *bYear;
+		strcpy(myDate.day, token1);
+		while (token1!= NULL) //while we split, we also store data in our struct
+		{
+
+			tokenCount++;
+
+			if(tokenCount == 3){
+				myDate.year = atoi(token1);
+				int yearVar = myDate.year;
+				int age = 2018 - yearVar;
+				if( yearVar <= 2000 && yearVar>=1993){
+
+
+					printf("%c.%s %d \n", c[i].name[0],  c[i].lastname,  age);
+
+				}
+			}
+			token1=strtok(NULL,"-");
+		}
+	}
 	return 0;
 }
